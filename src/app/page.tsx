1f3c24f9625f52b6e14c404d2a02422e83b70155
cherry2250 <kcr7812@naver.com>
2025-04-15 10:31:07 +0900

    settings : Architecture Settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./styles/page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>Container APM - Pulse</h1>
         <Image
           className={styles.logo}
           src="/next.svg"
